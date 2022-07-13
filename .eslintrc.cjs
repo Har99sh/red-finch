dab2a81c3e9aa53f4e018d6ecbad33b174e52cbe
luiquecruz <luiquecruz@gmail.com>
2022-07-13 16:17:13 -0300

    fix: add suppress naming rule for specific project case

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,9 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   "root": true,
   "rules": {
-    "vue/multi-word-component-names": 0
+    "vue/multi-word-component-names": [ "error", {
+      "ignores": [ "index", "[id]" ]
+    } ]
   },
   "extends": [
     "plugin:vue/vue3-essential",
