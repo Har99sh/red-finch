7b4ee7ea4e5f22f3f40968fb4f35715c969bc4d5
George Rykunov <dreennefor@gmail.com>
2022-07-14 00:07:57 +0500

    Updated to excercise 5

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import type { Task } from "@/types"
+// import {
+//     Card,
+//     CardTitle,
+//     CardHeader,
+//     Avatar,
+//     CardSubtitle
+// } from "@progress/kendo-vue-buttons"
+const props = defineProps({
+  task: Object,
+});
+</script>
+
+<template>
+  <div class="p-5 bg-white">
+    {{ task }}
+  </div>
+</template>
